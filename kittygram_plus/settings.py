e9c17be967816448ebeecbedcab73fd14b03a86b
Лев Андреев <leoohard@yandex.ru>
2023-03-04 21:54:06 +0300

    changes

--- kittygram_plus/settings.py
+++ kittygram_plus/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
+from datetime import timedelta
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -38,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
+    'djoser',
     'cats.apps.CatsConfig',
 ]
 
@@ -86,21 +88,22 @@ DATABASES = {
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
 
-AUTH_PASSWORD_VALIDATORS = [
-    {
-        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
-    },
-    {
-        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
-    },
-    {
-        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
-    },
-    {
-        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
-    },
-]
+REST_FRAMEWORK = {
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.IsAuthenticated', 
+    ],
 
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+    ],
+}
+
+
+SIMPLE_JWT = {
+    # Устанавливаем срок жизни токена
+    'ACCESS_TOKEN_LIFETIME': timedelta(days=1),
+    'AUTH_HEADER_TYPES': ('Bearer',),
+}
 
 # Internationalization
 # https://docs.djangoproject.com/en/3.2/topics/i18n/
--- kittygram_plus/urls.py
+++ kittygram_plus/urls.py
@@ -1,13 +1,27 @@
-from django.urls import include, path
-
 from rest_framework.routers import DefaultRouter
 
-from cats.views import CatViewSet
+from django.urls import include, path
+
+from cats.views import CatViewSet, LightCatViewSet, OwnerViewSet
 
 
 router = DefaultRouter()
 router.register('cats', CatViewSet)
+router.register('owners', OwnerViewSet)
+router.register(r'mycats', LightCatViewSet)
+# При регистрации эндпоинтов с таким URL-префиксом
+# router.register(r'profile/(?P<username>[\w.@+-]+)/', AnyViewSet)
+# ...вьюсет AnyViewSet будет получать на обработку все запросы с адресов
+# /profile/toh@/
+# /profile/nik.nik/
+# /profile/leo/
+# ...и подобных, ограниченных маской регулярного выражения.
 
 urlpatterns = [
     path('', include(router.urls)),
+    # Djoser создаст набор необходимых эндпоинтов.
+    # базовые, для управления пользователями в Django:
+    path('auth/', include('djoser.urls')),
+    # JWT-эндпоинты, для управления JWT-токенами:
+    path('auth/', include('djoser.urls.jwt')),
 ]
